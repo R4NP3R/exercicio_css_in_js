52b7e4964d7cb7d389b6c41170e11fce46f643a4
R4NP3R <ssrafa.2014@gmail.com>
2023-04-05 13:44:47 -0300

    Estilizando atraves do styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+import { ThemeProvider } from 'styled-components'
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import EstiloGlobal, { Container } from './styles'
+import MainTheme from './theme/mainTheme'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={MainTheme}>
+      <EstiloGlobal />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
-    </>
+      </Container>
+    </ThemeProvider>
   )
 }
 
---
+++ src/theme/mainTheme.ts
@@ -0,0 +1,11 @@
+const MainTheme = {
+  corPrincipal: '#a7727d',
+  corSecundaria: '#f9f5e7'
+}
+
+export default MainTheme
+
+export type Theme = {
+  corPrincipal: string
+  corSecudaria: string
+}
